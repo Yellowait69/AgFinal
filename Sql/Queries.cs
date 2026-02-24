c37d4d7f369d5bae83eca0d5eb6118366ed3f4da
maxen <etu52843@henallux.com>
2026-02-24 15:50:44 +0100

    ftyt

--- Sql/Queries.cs
+++ Sql/Queries.cs
@@ -5,94 +5,84 @@ namespace AutoActivator.Sql
 {
     public static class SqlQueries
     {
-        public static readonly ReadOnlyDictionary<string, string> Queries = new(new Dictionary<string, string>
+        public static readonly ReadOnlyDictionary<string, string> Queries = new ReadOnlyDictionary<string, string>(new Dictionary<string, string>
         {
             // ==========================================
             // RECUPERATION ID
             // ==========================================
 
-
-            { "GET_INTERNAL_ID", """
+            { "GET_INTERNAL_ID", @"
                 SELECT TOP 1 NO_CNT
                 FROM LV.SCNTT0 WITH(NOLOCK)
                 WHERE NO_CNT_EXTENDED = @ContractNumber
-                """ },
-
+                " },
 
             // ==========================================
             // DONNEES CONTRAT & AVENANTS
             // ==========================================
 
-            { "LV.SCNTT0", """
+            { "LV.SCNTT0", @"
                 SELECT * FROM LV.SCNTT0 WITH(NOLOCK)
                 WHERE NO_CNT = @InternalId
-                """ },
+                " },
 
-            { "LV.SAVTT0", """
+            { "LV.SAVTT0", @"
                 SELECT * FROM LV.SAVTT0 WITH(NOLOCK)
                 WHERE NO_CNT = @InternalId
                 ORDER BY NO_AVT ASC
-                """ },
-
+                " },
 
             // ==========================================
             // DONNEES PAIEMENTS
             // ==========================================
 
-            // Indispensable pour verifier que l'activation (le paiement) a bien ete prise en compte.
-            // On trie par date de reference et timestamp pour comparer l'historique comptable.
-            { "LV.PRCTT0", """
+            { "LV.PRCTT0", @"
                 SELECT * FROM LV.PRCTT0 WITH(NOLOCK)
                 WHERE NO_CNT = @InternalId
                 ORDER BY D_REF_PRM ASC, TSTAMP_CRT_RCT ASC
-                """ },
-
+                " },
 
             // ==========================================
             // DONNEES PRODUITS / GARANTIES
             // ==========================================
 
-            { "LV.SWBGT0", """
+            { "LV.SWBGT0", @"
                 SELECT * FROM LV.SWBGT0 WITH(NOLOCK)
                 WHERE NO_CNT = @InternalId
                 ORDER BY NO_AVT ASC, C_PROP ASC
-                """ },
-
+                " },
 
             // ==========================================
             // DONNEES BENEFICIAIRES & CLAUSES
             // ==========================================
 
-            { "LV.SCLST0", """
+            { "LV.SCLST0", @"
                 SELECT * FROM LV.SCLST0 WITH(NOLOCK)
                 WHERE NO_CNT = @InternalId
                 ORDER BY NO_AVT ASC, NO_ORD_CLS ASC
-                """ },
+                " },
 
-            { "LV.SCLRT0", """
+            { "LV.SCLRT0", @"
                 SELECT * FROM LV.SCLRT0 WITH(NOLOCK)
                 WHERE NO_CNT = @InternalId
                 ORDER BY NO_AVT ASC, NO_ORD_CLS ASC, NO_ORD_RNG ASC
-                """ },
-
+                " },
 
             // ==========================================
             // DONNEES FINANCIERES
             // ==========================================
 
-            // Tri par date d'abord, puis par sequence.
-            // Cela stabilise la comparaison si les sequences techniques changent mais pas la chronologie metier.
-            { "LV.BSPDT0", """
+            { "LV.BSPDT0", @"
                 SELECT * FROM LV.BSPDT0 WITH(NOLOCK)
                 WHERE NO_CNT = @InternalId
                 ORDER BY D_REF_MVT_EPA ASC, NO_ORD_TRF_EPA ASC, NO_ORD_MVT_EPA ASC
-                """ },
+                " },
 
-            { "LV.BSPGT0", """
+            { "LV.BSPGT0", @"
                 SELECT * FROM LV.BSPGT0 WITH(NOLOCK)
                 WHERE NO_CNT = @InternalId
                 ORDER BY D_REF_MVT_EPA ASC, NO_ORD_TRF_EPA ASC
-                """ }
+                " }
         });
     }
 }
